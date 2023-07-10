40027cf3fe2791d6bd45fd9cf187b91c9ac85ce5
smiethereens23nas <nicholas23smith@gmail.com>
2023-07-09 23:49:05 -0400

    added google auth sign on

--- Firebase/firebase.js
+++ Firebase/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,9 +16,12 @@ const firebaseConfig = {
   storageBucket: "lion-stix.appspot.com",
   messagingSenderId: "537842932203",
   appId: "1:537842932203:web:54db43aa2f32caae0ba1ae",
-  measurementId: "G-W34005ZSB8"
+  measurementId: "G-W34005ZSB8",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
